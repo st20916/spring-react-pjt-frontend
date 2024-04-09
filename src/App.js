a4024c46ddcb1984c3738d3994e72a3ab73d667f
이정재 <st20916@naver.com>
2024-04-09 15:57:17 +0900

    feat : Upload Front-End Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/test')
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        테스트 : {data}
+      </div>
     </div>
   );
 }
