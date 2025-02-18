c766e9b8791a1c46b4133d928fc1cebbeb47e6fa
이정재 <st20916@naver.com>
2025-02-18 23:07:09 +0900

    fix : add Router in Main

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import About from './router/about/About';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    axios.get('/api/test')
-        .then((res) => {
-          setData(res.data);
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-  });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        테스트 : {data}
-      </div>
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/about" element={<About />} />
+          </Routes>
+      </Router>
   );
 }
 
---
+++ src/router/about/About.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+    return <h1>소개 페이지</h1>;
+};
+
+export default About;
